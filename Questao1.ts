54d59296434f2a11cd9f3b3820350130f753cddc
nathan <nathanpss15@gmail.com>
2023-03-23 16:33:48 -0300

    Arquivos da Atv

---
+++ Questao1.ts
@@ -0,0 +1,11 @@
+
+type employee ={
+    code: Number,
+    name: String
+};
+
+let employee: employee ={
+    code: 10,
+    name: "Jonh"
+}
+
---
+++ Questao2.js
@@ -0,0 +1,16 @@
+"use strict";
+const pessoa1 = {
+    nome: 'maria',
+    idade: 29,
+    profissao: 'atriz',
+};
+const pessoa2 = {
+    nome: "roberto",
+    idade: 19,
+    profissao: "Padeiro",
+};
+const pessoa3 = {
+    nome: 'laura',
+    idade: "32",
+    profissao: "Atriz"
+};
